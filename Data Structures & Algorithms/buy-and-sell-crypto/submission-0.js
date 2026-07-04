0317bd6460da27001e99db5813ae093dd75f8242
jobinvarghese18 <52731772+jobinvarghese18@users.noreply.github.com>
2026-07-04 12:47:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+        for(let i=0; i<=prices.length;i++){
+            for(let j=i+1; j<=prices.length; j++){
+                if(maxProfit < prices[j]-prices[i]) maxProfit=prices[j]-prices[i]
+            }
+        }
+        return maxProfit
+    }
+}
