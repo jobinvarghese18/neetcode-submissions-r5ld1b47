c178b4a5e9abf82b7f65026c0323efa4d1507831
jobinvarghese18 <52731772+jobinvarghese18@users.noreply.github.com>
2026-07-28 23:37:46 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+         const result = [];
+         let prefix = 1;
+         let suffix = 1;
+    
+         for(let i=0; i<nums.length;i++){
+            result[i]=prefix;
+            prefix *=nums[i]
+         }
+
+         for(let i=nums.length-1; i>=0; i--){
+            result[i]= result[i]*suffix
+            suffix= nums[i]*suffix
+         }
+
+         return result;
+    }
+}
